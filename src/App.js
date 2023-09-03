233f7f3dba46605c7906fefd184b42aad15a1a3f
jade0819 <break0819@gmail.com>
2023-09-03 09:54:14 +0900

    Init: AWS S3 버킷 연결 및 deploy script 자동화 구축 완료

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello, Script!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          완전 자동 빌드!!!!!!!!!
         </a>
       </header>
     </div>
